660e9cebe21386101d0a1b01c7419157b68a5adc
jojo4042 <ryzetnt2.0@gmail.com>
2025-06-13 18:16:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -133,6 +133,9 @@ def index():
 
     return render_template('index.html', matches=matches, prediction=prediction)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
 
