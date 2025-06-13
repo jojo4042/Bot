633ecc52135b240ee299ce3415388f9353444c88
jojo4042 <ryzetnt2.0@gmail.com>
2025-06-13 03:17:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -41,3 +41,8 @@ def predict():
         return render_template("index.html", prediction=prediction, team1_score=team1_score, team2_score=team2_score)
     else:
         return render_template("index.html")
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
